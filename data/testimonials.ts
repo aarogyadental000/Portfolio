a10c40163a4e5d6b2a06eaba7e4e3fb5ee3b8d13
Surajj042 <suraj.gurung.sg98@gmail.com>
2026-08-12 13:18:46 +0545

    Added testimonial

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -4,6 +4,22 @@ export type Testimonial = {
   treatment?: string;
 };
 
-export const showTestimonials = false;
+export const showTestimonials = true;
 
-export const testimonials: Testimonial[] = [];
+export const testimonials: Testimonial[] = [
+  {
+    quote: "[PATIENT QUOTE 1]",
+    name: "[PATIENT NAME]",
+    treatment: "[TREATMENT]",
+  },
+  {
+    quote: "[PATIENT QUOTE 2]",
+    name: "[PATIENT NAME 2]",
+    treatment: "[TREATMENT 2]",
+  },
+  {
+    quote: "[PATIENT QUOTE 3]",
+    name: "[PATIENT NAME 3]",
+    treatment: "[TREATMENT 3]",
+  },
+];
