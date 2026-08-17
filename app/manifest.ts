bda7e50496e14b250d2d812c7ab6e16d19fa566d
Surajj042 <suraj.gurung.sg98@gmail.com>
2026-08-17 17:37:32 +0545

    .

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,9 +20,10 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/icon-maskable-512.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
